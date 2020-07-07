e7b5bef0224cdd7cc414c844e95811a7878eef26
Richard <66438599+defiancecp@users.noreply.github.com>
2020-07-07 15:07:03 -0700

    filters and sorts: formatting!
    lots and lots of formatting.

--- ServerFiles/domains/gtbikevroutes.fun/public_html/default.php
+++ ServerFiles/domains/gtbikevroutes.fun/public_html/default.php
@@ -51,24 +51,6 @@
    
 	// if a route is passed, go to details.  If it isn't, show summary.
     if($route === NULL) {
-		// Summary:
-		echo '<div style="color:#C0D0C8;"><table id="btnGrid">';
-		echo '<tr><td><input style="background-color:#303030;color:#C0D0C8;" type="text" id="myInRoute" onkeyup="fltFn()" placeholder="Named.." title="f1"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="text" id="myInAuthor" onkeyup="fltFn()" placeholder="By.." title="f2"></td>';
-		echo '<td></td><td></td>';
-		echo '<td colspan="2"><a href="default.php'.$metswtag.$routetag.'"><img src="'.$metsw.'" height="20px"/></a>'.$met.'</td></tr>';
-		echo '<tr><td><input style="background-color:#303030;color:#C0D0C8;" type="number" id="myDistMin" onkeyup="fltFn()" placeholder="at least Dist.." title="f5"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="number" id="myElvMin" onkeyup="fltFn()" placeholder="at least Elev.." title="f6"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="number" id="myRateMin" onkeyup="fltFn()" placeholder="at least rated.." title="f4"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="button" width="80px" height="10px" value="Dist ^" onclick="srtFn(9,1)"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="button" width="80px" height="10px" value="Elev ^" onclick="srtFn(11,1)"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="button" width="80px" height="10px" value="Rating ^" onclick="srtFn(8,1)"></td></tr>';
-		echo '<tr><td><input style="background-color:#303030;color:#C0D0C8;" type="number" id="myDistMax" onkeyup="fltFn()" placeholder="at most Dist.." title="f7"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="number" id="myElvMax" onkeyup="fltFn()" placeholder="at most Elev.." title="f8"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="text" id="myInType" onkeyup="fltFn()" placeholder="Type.." title="f3"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="button" width="80px" height="10px" value="Dist v" onclick="srtFn(9,-1)"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="button" width="80px" height="10px" value="Elev v" onclick="srtFn(11,-1)"></td>';
-		echo '<td><input style="background-color:#303030;color:#C0D0C8;" type="button" width="80px" height="10px" value="Rating v" onclick="srtFn(8,-1)"></td></tr></table>';
 
 //		echo '<input type="button" width="40px" height="10px" value="Type ^" onclick="srtFn(2,1)">';
 //		echo '<input type="button" width="40px" height="10px" value="Author ^" onclick="srtFn(1,1)"><br>';
@@ -76,8 +58,6 @@
 //		echo '<input type="button" width="40px" height="10px" value="Author v" onclick="srtFn(1,-1)">';
 // text sorting doesn't work ... and honestly doesn't seem important.
 
-        echo '';
-		echo '</div>';
         $sql = "CALL GetRouteData('".get_ip_address()."','ALL');";
         // execute SQL query to get all the route data.
         $conn = myConnection();
@@ -85,7 +65,38 @@
         if ($result = $conn->query($sql)) {
             if ($result->num_rows > 0) {
 				// now create a table to show results, and then cycle through each result row and build an html "tr" for each row and "td" for each displayed element.
-				echo '<table id="tblSummary" style="color:#a0b0a8;" class="table table-dark table-striped"><tr><th>Route</th><th>Author</th><th>Type</th><th>Distance</th><th>Elevation</th><th>Download</th><th>Map</th><th width="145px">Rating</th><th style="display:none;">numRating</th><th style="display:none;">numKM</th><th style="display:none;">numMI</th><th style="display:none;">numM</th><th style="display:none;">numFT</th></tr>'; // table opener & header row
+				echo '<table id="tblSummary" style="color:#a0b0a8;" class="table table-dark table-striped">';
+
+				// header row
+				echo '<tr><th>Route</th><th>Author</th><th>Type</th><th colspan="2">Distance</th><th colspan="2">Elevation</th><th>Download</th><th>Map</th><th colspan="2" width="145px">Rating</th><th style="display:none;">numRating</th><th style="display:none;">numKM</th><th style="display:none;">numMI</th><th style="display:none;">numM</th><th style="display:none;">numFT</th></tr>'; // table opener & header row
+
+				// Sort/Filter Controls:
+				echo '<tr id="rowSortFilter">';
+				echo '<th><input id="myInRoute" type="text" onkeyup="fltFn()" placeholder="search route.." title="f1"></th>';
+				echo '<th><input id="myInAuthor" type="text" onkeyup="fltFn()" placeholder="search author.." title="f2"></th>';
+				echo '<th><input id="myInType" type="text" onkeyup="fltFn()" placeholder="search type.." title="f3"></th>';
+
+				echo '<th><input id="myDistMin" type="number" onkeyup="fltFn()" placeholder=" Dist >" title="f5"><br>';
+				echo '<input id="myDistMax" onkeyup="fltFn()" placeholder="Dist <.." title="f7"></th>';
+
+				echo '<th><input id="btnDistAsc" type="button" value="^" onclick="srtFn(9,1)"><br>';
+				echo '<input id="btnDistDesc" type="button" value="v" onclick="srtFn(9,-1)"></th>';
+
+				echo '<th><input id="myElvMin" type="number" onkeyup="fltFn()" placeholder="Elev >.." title="f6"><br>';
+				echo '<input id="myElvMax" type="number" onkeyup="fltFn()" placeholder="Elev <.." title="f8"></th>';
+
+				echo '<th><input id="btnElvAsc" type="button" value="^" onclick="srtFn(11,1)"><br>';
+				echo '<input id="btnElvDesc" type="button" value="v" onclick="srtFn(11,-1)"></th>';
+
+				echo '<th colspan="2" >Units of Measure:<br><a href="default.php'.$metswtag.$routetag.'"><img src="'.$metsw.'" height="26px"/></a>'.$met.'</th>';
+
+				echo '<th><input stype="number" id="myRateMin" onkeyup="fltFn()" placeholder="Rated >.." title="f4"></th>';
+
+				echo '<th width=20px><input id="btnRtgAsc" type="button" value="^" onclick="srtFn(8,1)"><br>';
+				echo '<input id="btnRtgDesc" type="button" value="v" onclick="srtFn(8,-1)"></th>';
+				echo '</tr>';
+						
+				
                 // output data of each row
                 while($row = $result->fetch_assoc()) { // fetch is pop-like so each row is cycled through. when done, result of the assignment will be false, ending loop.
 					// this value contains the definition for each row's rating link URL.  See submitrating.php for implementation of this rating.
@@ -101,7 +112,7 @@
 					// but for now just fixing. This will probably be better in the API.
                     $mapstring = str_replace('Map</a>','<img src="images/map.png" class="link" height="20px"/></a>',$row["Map"]);
 					// now build the tr.
-                    echo '<tr><td><a class="link" href="default.php'.$mettag.'&route='.$row["RouteName"].'">'.$row["RouteName"].'</a></td><td>'.$row["Author"].'</td><td>'.$row["Type"].'</td><td>'.$vdistance.'</td><td>'.$velevation.'</td><td><a href="data:text/plain;charset=UTF-8,https://raw.githubusercontent.com/gtbikev/courses/master/courses/'.$row["RouteName"].'.json" download="'.$row["RouteName"].'.json"><img src=/images/dl.png class="link" height="20px"></a></td><td>'.$mapstring.'</td><td><iframe src="'.$ratinglink.'" class="embed-responsive-item" width="100%" height="20px" allowtransparency="true" style="border:0px solid black;"></iframe></td><td style="display:none;">'.$row["CurrentRating"].'</td><td style="display:none;">'.$row["DistanceKM"].'</td><td style="display:none;">'.$row["DistanceMI"].'</td><td style="display:none;">'.$row["ElevationM"].'</td><td style="display:none;">'.$row["ElevationFT"].'</td></tr>';
+                    echo '<tr><td><a class="link" href="default.php'.$mettag.'&route='.$row["RouteName"].'">'.$row["RouteName"].'</a></td><td>'.$row["Author"].'</td><td>'.$row["Type"].'</td><td colspan="2">'.$vdistance.'</td><td colspan="2">'.$velevation.'</td><td><a href="data:text/plain;charset=UTF-8,https://raw.githubusercontent.com/gtbikev/courses/master/courses/'.$row["RouteName"].'.json" download="'.$row["RouteName"].'.json"><img src=/images/dl.png class="link" height="20px"></a></td><td>'.$mapstring.'</td><td colspan="2"><iframe src="'.$ratinglink.'" class="embed-responsive-item" width="100%" height="20px" allowtransparency="true" style="border:0px solid black;"></iframe></td><td style="display:none;">'.$row["CurrentRating"].'</td><td style="display:none;">'.$row["DistanceKM"].'</td><td style="display:none;">'.$row["DistanceMI"].'</td><td style="display:none;">'.$row["ElevationM"].'</td><td style="display:none;">'.$row["ElevationFT"].'</td></tr>';
 				}
 					// all tr's done, close it up.
 				echo "</table>"; // table opener & header row
@@ -257,9 +268,9 @@ function srtFn(sIndex, sOrder) {
 	// sOrder: 1 asc, -1 desc
 
 	// sIndex:
-	// 8 - rating
-	// 9 - dist
-	// 11 - elv
+	// 10 	 - rating
+	// 11 - dist
+	// 13 - elv
 
 	// text options that don't work:
 	// 0 - name
@@ -272,25 +283,27 @@ function srtFn(sIndex, sOrder) {
 	while (switching) {
 		switching = false;
 		rows = table.rows;
-		for (i = 1; i < (rows.length - 1); i++) {
+		for (i = 0; i < rows.length; i++) {
 			shouldSwitch = false;
 			x = rows[i].getElementsByTagName("TD")[sIndex];
 			y = rows[i + 1].getElementsByTagName("TD")[sIndex];
-			if (sIndex >= 8) { // all the indexes above 8 are numeric
-				if ((x.innerHTML*sOrder) > (y.innerHTML*sOrder)) {
-					shouldSwitch = true;
-					break;
+			if(x && y) {
+				if (sIndex >= 8) { // all the indexes above 8 are numeric
+					if ((x.innerHTML*sOrder) > (y.innerHTML*sOrder)) {
+						shouldSwitch = true;
+						break;
+					};
+	// this is the broken text sort routine.
+				} else {
+					if(
+						(sOrder = -1 && x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase())
+						|| (sOrder = 1 && x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase())
+					) {
+						shouldSwitch = true;
+						break;
+					};
+	// end broken text sort routine.
 				};
-// this is the broken text sort routine.
-			} else {
-				if(
-					(sOrder = -1 && x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase())
-					|| (sOrder = 1 && x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase())
-				) {
-					shouldSwitch = true;
-					break;
-				};
-// end broken text sort routine.
 			};
 		};
 		if (shouldSwitch) {
